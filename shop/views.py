368d568a1f2eec2451591f71eeb606ae7de21849
sayan <sayanadh123@gmail.com>
2023-04-08 18:19:31 +0530

    designed the initial pipeline for the shop app

--- shop/views.py
+++ shop/views.py
@@ -3,4 +3,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'shop/index.html')
+
+
+def about(request):
+    return render(request, 'shop/index.html')
+
+
+def contact(request):
+    return render(request, 'shop/index.html')
+
+
+def tracker(request):
+    return render(request, 'shop/index.html')
+
+
+def search(request):
+    return render(request, 'shop/index.html')
+
+
+def productView(request):
+    return render(request, 'shop/index.html')
+
+
+def checkout(request):
+    return render(request, 'shop/index.html')
