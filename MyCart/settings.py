acfabe219842365d49606f2109264be90cd74714
sayan <sayanadh123@gmail.com>
2023-04-09 10:47:31 +0530

    added basic migrations and used html template inheritance

--- MyCart/settings.py
+++ MyCart/settings.py
@@ -119,6 +119,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+
+# I am editing this part below (MEDIA_ROOT and URL)
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.values_list('product_name')
+
+    products = [product[0].replace("'(),", "").title() for product in products]
+
+    params = {
+        'products': products
+    }
+    return render(request, 'shop/index.html', params)
 
 
 def about(request):
-    return render(request, 'shop/index.html')
+    return render(request, 'shop/about.html')
 
 
 def contact(request):
